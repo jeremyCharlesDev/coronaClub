397688f6fc12ce87115b553745d2731b207cc4e4
jeremyCharlesDev <jrmprod@gmail.com>
2020-03-18 13:53:08 +0100

    contact

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,6 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss']
 })
 export class HomePage {
+  team = {
+    name : 'Corona Club',
+    adress : 'Boulevard Pierre de Coubertin',
+    zipCode : '49000',
+    city : 'Angers',
+    phone : '06 00 00 00 00',
+    mail : 'the-bulls@mail.com',
+    logo : './../../assets/img/logo.png'
+  };
+
 
   constructor() {}
 

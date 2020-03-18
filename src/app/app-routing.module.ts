3728e2b0344f6032a43986f94ba66437ae2b4714
StephaneBouret <sbouret@orange.fr>
2020-03-18 14:33:30 +0100

    4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,11 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 
 const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   }
-
-=======
-
-const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  }
->>>>>>> 7a215f584cbc79a9784950259908df3a07dac1d9
 ];
 @NgModule({
   imports: [

c0c63751133257544b74b56ed0b544a4c8f413f3
StephaneBouret <sbouret@orange.fr>
2020-03-18 13:35:54 +0100

    modif envir.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },  {
+    path: 'login',
+    loadChildren: () => import('./home/login/login.module').then( m => m.LoginPageModule)
   },
+
 ];
 @NgModule({
   imports: [

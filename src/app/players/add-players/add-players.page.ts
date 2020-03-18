c156e541bad2c70f86a456aada0c9c2e44244ff9
leoc73 <leo.coiffard07@gmail.com>
2020-03-18 14:27:43 +0100

    Leo

---
+++ src/app/players/add-players/add-players.page.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { PlayersService } from 'src/app/services/players.service';
+import { Router } from '@angular/router';
+import { Player } from 'src/app/models/player.model';
+
+@Component({
+  selector: 'app-add-players',
+  templateUrl: './add-players.page.html',
+  styleUrls: ['./add-players.page.scss'],
+})
+export class AddPlayersPage implements OnInit {
+  newJoueur: Player = {
+    nom : '',
+    email: '',
+    tel: '',
+    prenom: '',
+    poste: '',
+    photo: '',
+  };
+
+  constructor(private playerService: PlayersService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+    addJoueur(nom: string, email: string, tel: string, prenom: string, poste: string, photo: string) {
+    this.newJoueur.nom = nom;
+    this.newJoueur.email = email;
+    this.newJoueur.tel = tel;
+    this.newJoueur.prenom = prenom;
+    this.newJoueur.poste = poste;
+    this.newJoueur.photo = photo;
+    console.log(this.newJoueur);
+    this.playerService.addPlayer(this.newJoueur).then(() => {
+      this.router.navigate(['/players']);
+    });
+    }
+
+    // addPhoto(event) {
+    //   this.playerService.uploadFile(event);
+    // }
+  }
